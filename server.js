d72857cc60a068b75332960c9291c7e2bc41637e
chenishi <chen.ishi@gmail.com>
2017-10-24 16:23:53 +0800

    testing nodejs by sending fname & lname ,and retrieve its response

--- server.js
+++ server.js
@@ -3,8 +3,14 @@ const app = express()
 const port = 61234
 
 app.listen(port,() => {
-    console.log('Listening on port ${port}')
+    console.log('Listening on port ${port}');
 
 })
 
 app.use(express.static(__dirname + '/public'))
+
+app.get('/get', function(req, res) {
+
+  res.send('<h1>Hello, ' + req.query.fname + req.query.lname + '</h1>');
+
+});
